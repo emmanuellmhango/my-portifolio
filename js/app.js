2b9b9cde52b355da5683efdeda76474a83639186
Emmanuel <emmanuellmhango@gmail.com>
2022-10-11 03:03:52 +0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,16 +3,16 @@ const menuModal = document.getElementById('myMenuModal');
 const menuClose = document.getElementById('closeBtn');
 const menuItems = document.getElementsByClassName('menu-links-item');
 
-menuClose.addEventListener('click', ()=> {
-    menuModal.style.display = 'none';
-    menu.style.display = 'block';
+menuClose.addEventListener('click', () => {
+  menuModal.style.display = 'none';
+  menu.style.display = 'block';
 });
 
-menu.addEventListener('click', ()=> {
-    menuModal.style.display = 'block';
-    menu.style.display = 'none';
+menu.addEventListener('click', () => {
+  menuModal.style.display = 'block';
+  menu.style.display = 'none';
 });
 
-menuItems.addEventListener('click', ()=> {
-    menu.style.display = 'none';
+menuItems.addEventListener('click', () => {
+  menu.style.display = 'none';
 });
