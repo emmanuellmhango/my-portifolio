30cc01067bbe2f63f88f642887cde2971323dbc9
Emmanuel <emmanuellmhango@gmail.com>
2022-10-11 03:59:08 +0400

    Update style.css, index.html, app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 const menu = document.getElementById('mobile-menu');
 const menuModal = document.getElementById('myMenuModal');
 const menuClose = document.getElementById('closeBtn');
-const menuItems = document.getElementsByClassName('menu-links-item');
+const menuItemAbout = document.getElementById('menu-links-item-about');
+const menuItemPortfolio = document.getElementById('menu-links-item-portfolio');
+const menuItemContact = document.getElementById('menu-links-item-contact');
 
 menuClose.addEventListener('click', () => {
   menuModal.style.display = 'none';
@@ -13,6 +15,17 @@ menu.addEventListener('click', () => {
   menu.style.display = 'none';
 });
 
-menuItems.addEventListener('click', () => {
-  menu.style.display = 'none';
+menuItemAbout.addEventListener('click', () => {
+  menuModal.style.display = 'none';
+  menu.style.display = 'block';
+});
+
+menuItemContact.addEventListener('click', () => {
+  menuModal.style.display = 'none';
+  menu.style.display = 'block';
+});
+
+menuItemPortfolio.addEventListener('click', () => {
+  menuModal.style.display = 'none';
+  menu.style.display = 'block';
 });
