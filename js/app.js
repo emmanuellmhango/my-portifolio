c7b0f823ef757a638591d4ccf1313f8710d26dcb
Emmanuel <emmanuellmhango@gmail.com>
2022-10-11 02:46:45 +0400

    Modified index.html, style.css, app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-const menu = document.getElementById('mobile-menu')
+const menu = document.getElementById("mobile-menu")
+const menuModal = document.getElementById("myMenuModal")
+const menuClose = document.getElementById("closeBtn")
+const menuItems = document.getElementsByClassName("menu-links-item")
+
+menuClose.addEventListener("click", function(){
+    menuModal.style.display = "none"
+    menu.style.display = "block"
+})
+
+menu.addEventListener("click", function(){
+    menuModal.style.display = "block"
+    menu.style.display = "none"
+})
+
+menuItems.addEventListener("click", function(){
+    menu.style.display = "none"
+})
