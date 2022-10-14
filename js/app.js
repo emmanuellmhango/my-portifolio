5559b5adba7738e94f3ef1faf440ee14d6e35d6e
Emmanuel <emmanuellmhango@gmail.com>
2022-10-14 20:31:57 +0400

    update app.js

--- js/app.js
+++ js/app.js
@@ -28,8 +28,8 @@ const projectData = {
     description: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting remaining essent', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea'],
     title: 'FACEBOOK',
     category: 'Back End Dev',
-    languages: ['html','css','javascript'],
-    images:['tonic-2-project.jpg','Snapshoot-Portfolio-3.png'],
+    languages: ['html', 'css', 'javascript'],
+    images: ['tonic-2-project.jpg','Snapshoot-Portfolio-3.png'],
   },
   multipostProjectData: {
     name: 'Multi-Post Stories', 
