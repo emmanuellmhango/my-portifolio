c44a1493091221f7c03a2826a8deae26b259a2bf
Cisco <ciscophrenzy@gmail.com>
2022-10-14 15:03:43 -0600

    Update script.js

--- js/app.js
+++ js/app.js
@@ -203,3 +203,20 @@ menuItemAbout.addEventListener('click', () => {
   menuHeaderTop.style.display = 'flex';
 });
 
+menuItemContact.addEventListener('click', () => {
+  menuModal.style.display = 'none';
+  menu.style.display = 'block';
+  menuBody.style.overflowY = 'visible';
+  menuBlur.style.filter = 'blur(0)';
+  menuLogo.style.filter = 'blur(0)';
+  menuHeaderTop.style.display = 'flex';
+});
+
+menuItemPortfolio.addEventListener('click', () => {
+  menuModal.style.display = 'none';
+  menu.style.display = 'block';
+  menuBody.style.overflowY = 'visible';
+  menuBlur.style.filter = 'blur(0)';
+  menuLogo.style.filter = 'blur(0)';
+  menuHeaderTop.style.display = 'flex';
+});
